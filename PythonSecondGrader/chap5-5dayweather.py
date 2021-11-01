ed664b59455b1c1fdfb10af7d5fb5469844f4239
Rui <kmri.nnnnn@gmail.com>
2021-11-01 18:15:45 +0900

    Finish chapter5

---
+++ PythonSecondGrader/chap5-5dayweather.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from pprint import pprint
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+# 5日間（3時間ごと）の天気予報を取得する：東京
+APIkey = "1d72e3eff33467bf9cb6f0ffc62eaf07"
+url = "http://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid={APIkey}&lang=ja&units=metric"
+cityname = "Tokyo,JP"
+url = url.format(cityname=cityname, APIkey=APIkey)
+
+jsondata = requests.get(url).json()
+# pprint(jsondata)
+# * データフレームを作成(項目名：気温)
+df = pd.DataFrame(columns=["気温"])
+tz = timezone(timedelta(hours=+9), 'JST')
+# * dt=タイムスタンプ(日時を数値で表したもの)
+# * dt_txt=日時
+for dat in jsondata["list"]:
+    # * UTCのタイムスタンプからタイムゾーン(JST)での日時を求める
+    jst = str(datetime.fromtimestamp(dat["dt"], tz))[:-9]
+    # UTCとJSTの日時を表示
+    # print("UTC={ust}, JST={jst}".format(ust=dat["dt_txt"], jst=jst))
+    weather = dat["weather"][0]["description"]
+    temp = dat["main"]["temp"]
+    # print("日時:{jst}, 天気:{w},気温:{t}度".format(jst=jst, w=weather, t=temp))
+    # * 日時をインデックスにして気温のデータを追加
+    df.loc[jst] = temp
+# pprint(df)
+df.plot(figsize=(15, 8))
+plt.ylim(-10, 40)
+plt.grid()
+plt.show()
+
+'''
+# UTC(協定世界時)をJST(日本標準時)に変換
+timestamp = 1562889600
+
+tz = timezone(timedelta(), 'UTC')
+utc = datetime.fromtimestamp(timestamp, tz)
+print(utc)
+
+tz = timezone(timedelta(hours=+9), 'JST')
+jst = datetime.fromtimestamp(timestamp, tz)
+print(jst)
+print(str(jst)[:-9])  # 後ろから9文字削除
+'''
---
+++ PythonSecondGrader/chap5-weather.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from pprint import pprint
+'''
+ans = "今日か{key1}です。明日は{key2}です。"
+print(ans)
+
+ans = ans.format(key1="晴れ", key2="曇り")
+print(ans)
+'''
+
+'''
+# 現在の天気を取得:都市名
+cityname = "Niigata,JP"
+url = "http://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}&lang=ja&units=metric"
+url = url.format(cityname=cityname,
+                 APIkey="1d72e3eff33467bf9cb6f0ffc62eaf07")
+'''
+# 現在の天気を取得:郵便番号
+zipcode = "160-0006,JP"
+url = "http://api.openweathermap.org/data/2.5/weather?zip={zipcode}&appid={APIkey}&lang=ja&units=metric"
+url = url.format(zipcode=zipcode,
+                 APIkey="1d72e3eff33467bf9cb6f0ffc62eaf07")
+jsondata = requests.get(url).json()
+
+# print(jsondata)
+# * 整形して表示
+# pprint(jsondata)
+print("都市名   = ", jsondata["name"])
+print("気温     = ", jsondata["main"]["temp"])
+print("天気     = ", jsondata["weather"][0]["main"])
+print("天気詳細 = ", jsondata["weather"][0]["description"])
+
+'''
+with open("PythonSecondGrader/test2.json", mode="r") as f:
+    jsondata = json.loads(f.read())
+    # pprint(jsondata)
+    print("1つ目のオブジェクト = ", jsondata[0])
+    print("都市名 = ", jsondata[0]["name"])
+    print("緯度   = ", jsondata[0]["coord"]["lat"])
+    print("経度   = ", jsondata[0]["coord"]["lon"])
+'''
